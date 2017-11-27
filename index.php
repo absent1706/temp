302b5cd0a3c0e544d82f2d480588025ac16327b5
Alexander Litvinenko <alitvinenko@toidev.com>
2017-11-27 15:16:12 +0200

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <p>lorem</p>
+    <p>lorem</p>
+    <p>lorem</p>
+    <?php 
+        echo 2+3;
+    ?>
+</body>
+</html>
